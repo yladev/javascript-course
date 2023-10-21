5e177a4236e6bf86f9c5d70b18535363c544bc5d
yladev <yanlinnaung881@gmail.com>
2023-10-21 11:25:14 +0630

    update airthmetic expressions

--- index.js
+++ index.js
@@ -23,3 +23,41 @@ console.log("Enrolled:", student);
 document.getElementById("p1").innerHTML = "Hello " + firstName;
 document.getElementById("p2").innerHTML = "You are " + age + " years old";
 document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+/**
+ * arithmetic expression is a combination of...
+ * operands (values, cariables, etc.)
+ * operators (+ - * / %)
+ * that can be evaluated to a value
+ * ex. y = x + 5;
+ */
+
+let students = 21;
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+console.log(students);
+
+let extraStudents = students % 2;
+console.log(extraStudents);
+
+// augmented assignment operator
+let boys = 20;
+
+boys += 1;
+boys -= 1;
+boys *= 2;
+boys /= 2;
+console.log(boys);
+
+/**
+ * operator precedence
+ * parenthesis ()
+ * exponents
+ * multiplication & division
+ * addition & subtraction
+ */
+
+let result = 1 + 2 * (3 + 4);
+console.log(result);
