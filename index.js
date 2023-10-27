86f2822758b1612696212db565835a117f5d21c1
yladev <yanlinnaung881@gmail.com>
2023-10-27 20:53:37 +0630

    update const

--- index.js
+++ index.js
@@ -94,3 +94,18 @@ z = Boolean("pizza");
 console.log(x, typeof x);
 console.log(y, typeof y);
 console.log(z, typeof z);
+
+// const = a variable that can't be changed
+
+let pi = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+pi = 420.69;
+
+circumference = 2 * pi * radius;
+
+console.log("The circumference is: => ", circumference);
