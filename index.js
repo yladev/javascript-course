7cb26a342a46d0f0505ea9ab9c2d250ec0b1741e
yladev <yanlinnaung881@gmail.com>
2023-10-28 16:44:01 +0630

    update math

--- index.js
+++ index.js
@@ -109,3 +109,22 @@ pi = 420.69;
 circumference = 2 * pi * radius;
 
 console.log("The circumference is: => ", circumference);
+
+// Math
+
+let num1 = 3.14;
+let num2 = 5;
+let num3 = 9;
+let maximum;
+let minimum;
+// num1 = Math.round(num1);
+// num1 = Math.floor(num1);
+// num1 = Math.ceil(num1);
+// num1 = Math.pow(num1, 3);
+// num1 = Math.sqrt(num1);
+// num1 = Math.abs(num1);
+maximum = Math.max(num1, num2, num3);
+minimum = Math.min(num1, num2, num3);
+num1 = Math.PI;
+
+console.log(num1);
